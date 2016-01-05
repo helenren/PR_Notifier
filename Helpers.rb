68d69380bb75a73bbf0377838dfebe4edd7ce148
Qingchun Ren <qren@internal.salesforce.com>
2016-01-04 22:39:10 -0800

    fix tha issue that not do notify for the expired pr that has no any comments yet

--- Helpers.rb
+++ Helpers.rb
@@ -1,13 +1,17 @@
 module Helpers
 # poll expired pull request loop
-def Helpers.every_n_seconds(n)
-  begin
-    loop do
-      before = Time.now
-      yield
-      interval = n-(Time.now-before)
-      sleep(interval) if interval > 0
-    end
+  def Helpers.every_n_seconds(n)
+    begin
+      loop do
+        before = Time.now
+        yield
+        interval = n-(Time.now-before)
+        sleep(interval) if interval > 0
+      end
   end
 end
+
+  def Helpers.check_expired(pr_created_time, timeout)
+    (Time.now - pr_created_time) > timeout.to_i
   end
+end
--- PR_Monitor.rb
+++ PR_Monitor.rb
@@ -39,17 +39,16 @@ Helpers.every_n_seconds(config[:github_polling_interval_seconds].to_i) do
   client.pull_requests(repo, :state => 'open').collect {|pull_request| pull_request.number}.each do |pull_request_id|
     open_pull_request = client.pull_request(repo, pull_request_id)
 
-    if (open_pull_request[:title].include? "[Ready for Review]")
+    if (open_pull_request[:title].include? config[:pull_request_ready_indicator])
       # all comments returned by the created time order, the latest one is on the top
       client.pull_request_comments(repo, pull_request_id).sort{|a,b| b[:created_at] <=> a[:created_at]}.each do |pr_comment|
         if (open_pull_request[:user][:login] != pr_comment[:user][:login])
-          pr_expired = (Time.now - pr_comment[:created_at]) > config[:pull_request_timeout_seconds].to_i
+          pr_expired = Helpers.check_expired(pr_comment[:created_at], config[:pull_request_timeout_seconds])
           not_alarmed = true
           alarm_comment_id = 0
           client.issue_comments(repo, pull_request_id).each do |iss_comment|
             content = iss_comment[:body]
             if content.include? config[:notification_comments]
-              puts "2"
               not_alarmed = false
               alarm_comment_id = iss_comment[:id]
             end
@@ -70,6 +69,21 @@ Helpers.every_n_seconds(config[:github_polling_interval_seconds].to_i) do
           #find the latest comments not from pull request author update the pull request expiration status based on it
           #skip other comments check
           break
+        end
+        pr_expired = Helpers.check_expired(open_pull_request[:created_at], config[:pull_request_timeout_seconds])
+        if (pr_expired)
+          not_alarmed = true
+          client.issue_comments(repo, pull_request_id).each do |iss_comment|
+            content = iss_comment[:body]
+            if content.include? config[:notification_comments]
+              not_alarmed = false
+              break
+            end
+          end
+          if (not_alarmed)
+            #add expiration comment to the pull request that has no any comments yet
+            client.add_comment(repo, pull_request_id, "#{config[:notification_comments]}")
+          end
         end
       end
     end
